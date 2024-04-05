a2c070f515d3e7f8fec3961541ca38e77658ba41
George Mihov <53583283+gmihov001@users.noreply.github.com>
2024-04-05 19:35:59 +0000

    Sorting by string, boolean and Date

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,13 +10,6 @@ const Home = () => {
 		setSortMe(() => (a, b) => 0);
 	}, [])
 
-
-	const addTask = (e) => {
-		let date = new Date();
-		setTasks([...tasks, { title: newTask, done: false, date: date.toLocaleString() }, { title: "Clean", done: true, date: date.toLocaleString() }]);
-		setNewTask("");
-	}
-
 	const sortTitles = (a, b) => {
 		let nameA = a.title.toUpperCase();
 		let nameB = b.title.toUpperCase();
@@ -28,6 +21,33 @@ const Home = () => {
 		if (a.done && !b.done) return -1;
 		if (!a.done && b.done) return 1;
 		else return 0;
+	}
+
+	const sortDates = (a, b) => a.date - b.date;
+
+
+	const addTask = (e) => {
+		let date = new Date();
+		setTasks([...tasks, {
+			id: Math.floor(Math.random() * 99999999),
+			title: newTask,
+			done: false,
+			date: date.toLocaleString()
+		}]);
+		setNewTask("");
+	}
+
+	const markDone = (id) => {
+		let updatedTasks = tasks.map((task) => {
+			if (task.id == id) return { ...task, done: !task.done };
+			else return task;
+		});
+		setTasks(updatedTasks);
+	}
+
+	const deleteTask = (id) => {
+		let updatedTasks = tasks.filter((task) => task.id != id);
+		setTasks(updatedTasks);
 	}
 
 
@@ -51,16 +71,25 @@ const Home = () => {
 					</button>
 					<ul className="dropdown-menu">
 						<li><a className="dropdown-item" onClick={() => setSortMe(() => sortTitles)} href="#">By title</a></li>
-						<li><a className="dropdown-item" href="#">By date</a></li>
+						<li><a className="dropdown-item" onClick={() => setSortMe(() => sortDates)} href="#">By date</a></li>
 						<li><a className="dropdown-item" onClick={() => setSortMe(() => sortStatuses)} href="#">By status</a></li>
 					</ul>
 				</div>
+
 			</div>
 			<div>
 				{
 					tasks.toSorted(sortMe).map((task, ind) => (
 						<div key={ind} className="alert alert-info shadow p-3 m-2 bg-body rounded d-flex justify-content-between" role="alert">
 							<strong>{task.title}</strong> <small>{task.date.toString()}</small>
+							<div className="d-flex">
+								<span
+									onClick={() => markDone(task.id)}
+									className="input-group-text"><i className="far fa-check-square"></i></span>
+								<span
+									onClick={() => deleteTask(task.id)}
+									className="input-group-text"><i className="far fa-times-circle"></i></span>
+							</div>
 						</div>
 					))
 				}
